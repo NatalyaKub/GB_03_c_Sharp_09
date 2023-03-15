f3d028e2eb66315830183d40ca620f6372b0c6da
Natalyae <ngk.s@mail.ru>
2023-03-15 21:20:42 +0100

    Add code hw_01

--- hw_01/Program.cs
+++ hw_01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void GetNum(int num){
+    Console.Write($"{num}, ");
+    if(num == 1) return;
+    GetNum (num - 1);
+}
+
+Console.WriteLine("Введите N:");
+int n = Convert.ToInt32(Console.ReadLine());
+GetNum(n);
