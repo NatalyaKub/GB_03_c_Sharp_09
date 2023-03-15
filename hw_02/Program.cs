fc358ee51b1a952ffaf16efc472dfb4d571a9a69
Natalyae <ngk.s@mail.ru>
2023-03-15 21:38:34 +0100

    Add code hw_02

--- hw_02/Program.cs
+++ hw_02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNum(int m, int n){
+    //Console.Write($"{num}, ");
+    if(n == m) {
+        return m;
+    }
+    return n + SumNum(m, n - 1);
+}
+
+Console.WriteLine("Введите M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int sum = SumNum(m,n);
+Console.WriteLine($"Сумма от {m} до {n}: {sum}");
